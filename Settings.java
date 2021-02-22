9090189b7a1611de32dcc1f251f64a652380eaff
Egor Fedorenko <68076283+h2sm@users.noreply.github.com>
2021-02-22 18:58:59 +0300

    Add files via upload

--- Settings.java
+++ Settings.java
@@ -4,22 +4,20 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class Settings implements HelloMXBean{
     private HashMap<String,ScheduledFuture> scheduledList = new HashMap<>();//список заданий
-    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);//планировщик
     private ClassLoader loader;
 
     @Override
     public void submit(String name, String classpath, String mainClass, int period) throws Exception {
-        ScheduledFuture scheduledFuture = scheduler.scheduleAtFixedRate(new TaskManager(classpath, mainClass), 0, period, TimeUnit.SECONDS);
-        scheduledList.put(name, scheduledFuture);
+        ScheduledFuture schedule = Executors.newScheduledThreadPool(1).
+                scheduleAtFixedRate(new TaskManager(classpath, mainClass), 0, period, TimeUnit.SECONDS);//планировщик
+        scheduledList.put(name, schedule);
     }
 
     @Override
@@ -29,7 +27,7 @@ public class Settings implements HelloMXBean{
 
     @Override
     public String status(String name) {
-        if (scheduledList.get(name).isCancelled()) return "cancelled";//доделать!
+        return "is cancelled " + scheduledList.get(name).isCancelled();//доделать!
     }
     private ClassLoader newLoader(String dir) throws Exception {
         var path = Path.of(dir);
